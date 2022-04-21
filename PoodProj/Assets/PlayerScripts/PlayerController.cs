38e1c88c48e070784d9067d8dcca7bd0fcddec1a
James <jstricker99@gmail.com>
2022-04-20 20:44:07 -0400

    Player zoom functionality fixed for each level

--- PoodProj/Assets/PlayerScripts/PlayerController.cs
+++ PoodProj/Assets/PlayerScripts/PlayerController.cs
@@ -6,12 +6,18 @@ using Cinemachine;
 public class PlayerController : MonoBehaviour
 {
     public CinemachineVirtualCamera vcam;
+    public GameObject scopeOverlay;
     //the maximum or 'base' FOV
     float maxFOV = 60f;
     //the minimum or 'zoom' FOV    //Numbers on both FOVs are placeholder until gun is integrated
     float minFOV = 15f;
     float zoomInput;
     float shootInput;
+
+    public void Start()
+    {
+        scopeOverlay.SetActive(false);
+    }
     
 
     //passes zoomInput in from InputController whenever the zoom event updates
@@ -21,9 +27,11 @@ public class PlayerController : MonoBehaviour
         //sets the FOV to the default max and then checks if the zoom button is pressed
         //if the zoom button is pressed, then it sets the FOV to the zoom FOV
         vcam.m_Lens.FieldOfView = maxFOV;
+        scopeOverlay.SetActive(false);
         if (zoomInput == 1)
         {
             vcam.m_Lens.FieldOfView = minFOV;
+            scopeOverlay.SetActive(true);
         }
         Debug.Log($"Player Controller Zoom Input:{zoomInput}");
     }
